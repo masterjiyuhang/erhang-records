6a9e0cc99ecff922a093e45d8ace82e670cb59ce
纪宇航 <jiyuhang@jctrans.net>
2023-07-14 18:20:33 +0800

    feat: update

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   base: '/erhang-records/',
-  title: "ErHang Records",
-  description: "A VitePress Site",
+  title: 'ErHang Records',
+  description: 'A VitePress Site',
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
@@ -18,13 +18,31 @@ export default defineConfig({
         items: [
           { text: 'Markdown Examples', link: '/markdown-examples' },
           { text: 'Runtime API Examples', link: '/api-examples' },
-           { text: 'Generate A React Template', link: '/study-react' }
+          { text: 'Generate A React Template', link: '/study-react' }
+        ]
+      },
+      {
+        text: 'Interview-related',
+        items: [
+          {
+            text: '基础篇',
+            items: [{ text: '模块化', link: '/JavaScript/l1' }]
+          },
+          {
+            text: 'Browser Related Knowledge',
+            items: [
+              { text: 'Rendering Process', link: '/browser/l1' },
+              { text: 'Rendering Optimization', link: '/browser/l2' }
+            ]
+          },
+          {
+            text: '框架篇',
+            items: [{ text: 'Vue', link: '/framework/vue' }]
+          }
         ]
       }
     ],
 
-    socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
-    ]
+    socialLinks: [{ icon: 'github', link: 'https://github.com/vuejs/vitepress' }]
   }
 })
