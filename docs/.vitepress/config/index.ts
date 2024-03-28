6dc6619e38e87df27b88177aa1039ced7856dc69
纪宇航 <jiyuhang@jctrans.net>
2024-03-28 10:52:44 +0800

    feat: 添加 clarity 站点分析

--- docs/.vitepress/config/index.ts
+++ docs/.vitepress/config/index.ts
@@ -41,6 +41,15 @@ export default defineConfig({
     },
   },
   head: [
+    [
+      'script',
+      { type: 'text/javascript' },
+      ` (function(c,l,a,r,i,t,y){
+      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+      t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+      y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+  })(window, document, "clarity", "script", "lnisp1k6mx");`,
+    ],
     // ['link', { rel: 'icon', type: 'image/svg+xml', href: '/erhang-records/logo.svg' }],
     [
       'link',
